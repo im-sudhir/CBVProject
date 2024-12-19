bede114aa20ef5e657678787bd2a6d7c6c466228
im-sudhir <ssudhir794@gmail.com>
2024-12-19 20:22:04 +0530

    added model and serializer

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class CourseModel(models.Model):
+    name =models.CharField(max_length=40)
+    author=models.CharField(max_length=40)
+    price=models.IntegerField()
+    discount=models.FloatField(default=0)
+    duration=models.DurationField()
+
+    def __str__(self):
+        return self.name
