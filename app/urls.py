21c1279069ed94259008eb25dcbae182adefd3bf
im-sudhir <ssudhir794@gmail.com>
2024-12-20 18:59:09 +0530

    added primary key based operations(GET,PUT,DELETE)

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import CourseListView
+from .views import CourseListView,CourseDetailView
 
 urlpatterns=[
     path('courses', CourseListView.as_view()),
+    path('courses/<int:pk>', CourseDetailView.as_view()),
+
 ]
--- app/views.py
+++ app/views.py
@@ -20,5 +20,35 @@ class CourseListView(APIView):
         else:
             return Response(serializer.errors)
 
+class CourseDetailView(APIView):
+    def get(self,request, pk):
+        try:
+            course=CourseModel.objects.get(pk=pk)
+            serializer=CourseSerializer(course)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except CourseModel.DoesNotExist:
+            return Response({"message":"Not Found"},status=status.HTTP_404_NOT_FOUND)
 
+    def put(self, request, pk):
+        try:
+            # Retrieve the existing instance
+            course = CourseModel.objects.get(pk=pk)
+        except CourseModel.DoesNotExist:
+            return Response({"error": "Course not found"}, status=status.HTTP_404_NOT_FOUND)
 
+        # Pass the existing instance to the serializer
+        serializer = CourseSerializer(course, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    
+    def delete(self,request,pk):
+        try:
+            course = CourseModel.objects.get(pk=pk)
+            course.delete()
+            return Response({"message": "Course deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+        except CourseModel.DoesNotExist:
+            return Response({"error": "Course not found"}, status=status.HTTP_404_NOT_FOUND)
